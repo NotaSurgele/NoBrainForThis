96115661d0711ac4099d8fd1f90e3e97a4b97f7f
NotaSurgele <alexis.picard@epitech.eu>
2021-07-12 21:01:23 +0200

    can retrieve password but latency of 1 reload

--- app.js
+++ app.js
@@ -1,20 +1,40 @@
-var list = [];
 var createPass = `<button id="generatedPassword"> Generate password </button>`;
-var getTab;
 
 chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT},
-    getTab = function getcurrentTab(tabs){
-        current = tabs[0].url;
-        console.log(current);
-        return current;
-    }
-);
+function(tabs) {
+    getcurrentURL(tabs);
+});
+
+function getcurrentURL(tabs)
+{
+    let url = tabs[0].url;
+    localStorage.setItem("currentURL", url);
+}
 
 function checkIfPasswordExist() {
+    var list = [];
+    list = JSON.parse(localStorage.getItem("stockedData"));
 
-    this.list = localStorage.getItem("stockedData");
-    if (this.list == null || this.list == undefined) {
-        document.getElementById('content').innerHTML = this.createPass;
+    if (list == null || list == undefined) {
+        document.getElementById('generate').innerHTML = createPass;
+    } else {
+        let length = Object.keys(list).length;
+
+        for (let i = 0; i != length; i++) {
+            var str = JSON.stringify(list[i]);
+
+            if (str.indexOf(localStorage.getItem("currentURL")) != -1) {
+                var passwd = str.split('§');
+                console.log(str);
+                console.log("Your password for this site is: " + passwd[1]);
+                document.getElementById("refresh").innerHTML = `<button id="ref"> refresh </button>`
+                document.getElementById("refresh").addEventListener("click", function() {
+                    document.location.reload();
+                });
+                return;
+            }
+        }
+        document.getElementById('generate').innerHTML = createPass;
     }
 }
 
@@ -23,7 +43,7 @@ function algorithm() {
     var letterLower = "abcdefghijklmnopqrstuvwxyz";
     var letterUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     var digit = "0123456789";
-    var specialChar = "!:;,(-_)=}]@^\|[{~#²";
+    var specialChar = "!:;,(-_)=}]@^\|[{#²";
 
     for (let i = 0; i != 24; i++) {
         if (i < 6) {
@@ -36,7 +56,7 @@ function algorithm() {
             let value = Math.floor(Math.random() * 9);
             passWord += digit[value];
         } else {
-            let value = Math.floor(Math.random() * 19);
+            let value = Math.floor(Math.random() * 18);
             passWord += specialChar[value];
         }
     }
@@ -47,12 +67,26 @@ function algorithm() {
 function showGeneratedPassword()
 {
     var password = algorithm();
-    console.log("password = " + password);
+    
     document.getElementById('password').innerText = password;
+    var keep = document.getElementById('keep');
+    if (keep != null) {
+        keep.innerHTML = `<button id="keepThisOne"> Save for this site </button>`;
+    }
+    keep.addEventListener('click', function() {
+        var list = JSON.parse(localStorage.getItem("stockedData"));
+        if (list == null)
+            list = [];
+        var currentURL = localStorage.getItem("currentURL");
+        list.push(JSON.stringify(currentURL + "§" + password));
+        localStorage.setItem("stockedData", JSON.stringify(list));
+        window.location.reload();
+    });
 }
 
 function generatePassword() {
-    document.getElementById('generatedPassword').addEventListener('click', showGeneratedPassword);
+    if (document.getElementById('generatedPassword'))
+        document.getElementById('generatedPassword').addEventListener('click', showGeneratedPassword);
 }
 
 function main() {
@@ -60,4 +94,8 @@ function main() {
     generatePassword();
 }
 
-window.onload = main();
+if (document.onvisibilitychange) {
+    window.location.reload();
+}
+
+document.onload = main();
